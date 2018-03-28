d4d596700d9675d606e63fb49808ad86e16602e4
WaylonFocus <songwenlue@gmail.com>
2018-03-28 16:51:45 +0800

    entry update

--- entry/goodsAll.js
+++ entry/goodsAll.js
@@ -1,6 +1,7 @@
 require("../components/goods/goodsManagement");
 require("../components/goods/officeIssue");
 require("../components/goods/officePurchase");
+require("../components/goods/officeHistory");
 require("../components/goods/yunyingbuIssue");
 require("../components/goods/yunyingbuPurchase");
 require("../components/goods/yunyingbuHistory");
